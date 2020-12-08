465523079d39b8654a68203ed982da59470fa194
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-08 23:10:01 +0100

    feat :  association related tables

--- accounts/models.py
+++ accounts/models.py
@@ -16,7 +16,7 @@ from .managers import CustomUserManager
 
 # Create your models here.
 class BaseUser(AbstractUser):
-    
+
     key = models.UUIDField(primary_key=True, default=uuid.uuid4)
 
     first_name = models.CharField(_("first name"), max_length=150, null=True)
@@ -65,3 +65,63 @@ class BaseUser(AbstractUser):
 
     def __str__(self):
         return str(self.email)
+
+
+class AssociationType(models.Model):
+    name = models.CharField(_('name of the association'), max_length=125)
+    description = models.TextField(_('description of the association'),
+                                   max_length=750)
+
+
+class ExpectedAssociationMembersNumber(models.Model):
+    max_number = models.IntegerField(
+        _('expected max number of association members number'))
+    min_number = models.IntegerField(
+        _('expected min number of association members number'))
+
+
+class Association(models.Model):
+    name = models.CharField(_('name of the association'), max_length=225)
+    description = models.TextField(_('description of the association'),
+                                   max_length=1500)
+    association_type = models.ForeignKey(
+        AssociationType,
+        verbose_name=_('the association type'),
+        on_delete=models.CASCADE)
+    association_min_max_numbers = models.ForeignKey(
+        ExpectedAssociationMembersNumber,
+        verbose_name=_(
+            'expected max and min number of association members number'),
+        on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Member (models.Model):
+    user = models.ForeignKey(BaseUser, verbose_name=_("member"), on_delete=models.CASCADE)
+    association = models.ForeignKey(Association , verbose_name=_("association"), on_delete=models.CASCADE)
+    is_owner = models.BooleanField(_('is an association owner'), default=False)
+    is_archived = models.BooleanField(_('this user is not a member any more'), default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+class AssociationGroup(models.Model):
+    STATIQUE = 'S'
+    DYNAMIQUE = 'D'
+
+    ASSOCIATION_GROUP_TYPE = [(STATIQUE, 'STATIQUE'), (DYNAMIQUE, 'DYNAMIQUE')]
+    name = models.CharField(_('name of the group'), max_length=225)
+    association = models.ForeignKey(Association , verbose_name=_("association"), on_delete=models.CASCADE)
+    group_type = models.CharField(
+        max_length=2,
+        choices=ASSOCIATION_GROUP_TYPE,
+        default=STATIQUE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class AssociationGroupMember(models.Model):
+    member = models.ForeignKey(Member, verbose_name=_("member"), on_delete=models.CASCADE)
+    group = models.ForeignKey(AssociationGroup, verbose_name=_("association group"), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
