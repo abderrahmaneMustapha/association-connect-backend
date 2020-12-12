10fc0d26c4c483cc04367ba4a0d166f2e911de65
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-12 16:58:01 +0100

    test : delete and update association description

--- accounts/schema.py
+++ accounts/schema.py
@@ -1,5 +1,7 @@
 from graphene_django import DjangoObjectType
-from .models import BaseUser,Member, Association, AssociationGroup, AssociationGroupMember
+from .models import (BaseUser, Member, Association, AssociationGroup,
+                     AssociationGroupMember, AssociationType as AssociationTypeModel,
+                     ExpectedAssociationMembersNumber)
 import graphene
 
 
@@ -14,7 +16,7 @@ class AssociationType(DjangoObjectType):
     class Meta:
         model = Association
         fields = [
-            'name', 'description', 'association_type',
+            'id','name', 'description', 'association_type',
             'association_min_max_numbers'
         ]
 
@@ -22,12 +24,14 @@ class AssociationType(DjangoObjectType):
 class AssociationGroupType(DjangoObjectType):
     class Meta:
         model = AssociationGroup
-        fields = ['name', 'association', 'group_type']
+        fields = ['id', 'name', 'association', 'group_type']
+
 
 class AssociationGroupMemberType(DjangoObjectType):
     class Meta:
         model = AssociationGroupMember
         fields = ['member', 'group']
+
 
 #end types
 
@@ -63,11 +67,12 @@ class MemberAddByAdminMutation(graphene.Mutation):
 
         association = Association.objects.get(id=association)
         user_ = BaseUser.objects.get(key=user)
-        member = Member.objects.create(association=association,
-                                       user=user_)
+        member = Member.objects.create(association=association, user=user_)
         success = True
 
         return MemberAddMutation(member=member, success=success)
+
+
 class MemberDeleteMutation(graphene.Mutation):
     class Arguments:
         user = graphene.String()
@@ -76,12 +81,13 @@ class MemberDeleteMutation(graphene.Mutation):
     success = graphene.Boolean()
     member = graphene.Field(MemberType)
 
-    def mutate(root, info, association, user ):      
+    def mutate(root, info, association, user):
 
         member = Member.objects.filter(association__pk=association,
                                        user__key=user).delete()
         success = True
         return MemberDeleteMutation(member=member, success=success)
+
 
 class MemberArchiveMutation(graphene.Mutation):
     class Arguments:
@@ -93,29 +99,33 @@ class MemberArchiveMutation(graphene.Mutation):
 
     def mutate(root, info, association, user):
 
-
         member = Member.objects.get(association__pk=association,
-                                       user__key=user)
+                                    user__key=user)
         member.is_archived = True
         member.save()
         success = True
         return MemberAddMutation(member=member, success=success)
 
-class AssciationCreationMutation(graphene.Mutation):
 
+class AssociationCreationMutation(graphene.Mutation):
     class Arguments:
         name = graphene.String()
         description = graphene.String()
         association_type = graphene.ID()
         association_min_max_numbers = graphene.ID()
         phone = graphene.String()
-        email  =  graphene.String()
+        email = graphene.String()
 
     success = graphene.Boolean()
     association = graphene.Field(AssociationType)
 
     def mutate(root, info, name, description, association_type,
                association_min_max_numbers, phone, email):
+
+        association_type = AssociationTypeModel.objects.get(id=association_type)
+
+        association_min_max_numbers = ExpectedAssociationMembersNumber.objects.get(
+            id=association_min_max_numbers)
 
         association = Association.objects.create(
             name=name,
@@ -127,61 +137,79 @@ class AssciationCreationMutation(graphene.Mutation):
         user.is_association_owner = True
         user.save()
 
-        Member.objects.create(user=user, association=association,  is_owner=True)
+        Member.objects.create(user=user,
+                              association=association,
+                              is_owner=True)
 
         success = True
 
-        return AssciationCreationMutation(association=association,
-                                          success=success)
+        return AssociationCreationMutation(association=association,
+                                           success=success)
 
-class AssciationDeleteMutation(graphene.Mutation):
+
+class AssociationDeleteMutation(graphene.Mutation):
     class Arguments:
         id = graphene.ID()
-    
+
     success = graphene.Boolean()
     association = graphene.Field(AssociationType)
 
     def mutate(root, info, id):
-        association = Association.objects.get(pk=id)
+        association = Association.objects.filter(id=id)
+        association_ = association.first()
+        association.delete() 
         success = True
-        return AssciationDeleteMutation(association=association, success=success)
+        return AssociationDeleteMutation(association=association_, success=success)
+
+
 class AssociationUpdateDescriptionMutation(graphene.Mutation):
     class Arguments:
+        association = graphene.ID()
         description = graphene.String()
 
     success = graphene.Boolean()
     association = graphene.Field(AssociationType)
 
-    def mutate(root, info, description):
-        assciation = Association.objects.update(description=description)
+    def mutate(root, info, association, description):
+        association = Association.objects.filter(id=association)
+        association.update(description=description)
         success = True
-        return AssciationCreationMutation(assciation=assciation,
-                                          success=success)
+        return AssociationCreationMutation( association=association.first(),
+                                           success=success)
+
+
 class AssociationGroupCreationMutation(graphene.Mutation):
     class Arguments:
         name = graphene.String()
         association = graphene.ID()
         group_type = graphene.String()
-    success =  graphene.Boolean()
-    group  = graphene.Field(AssociationGroupType)
+
+    success = graphene.Boolean()
+    group = graphene.Field(AssociationGroupType)
 
     def mutate(root, info, name, association, group_type):
-        _group = AssociationGroup.objects.create(name=name, association=association, group_type=group_type)
+        _group = AssociationGroup.objects.create(name=name,
+                                                 association=association,
+                                                 group_type=group_type)
 
         return AssociationGroupCreationMutation(group=_group, success=success)
+
 
 class AssociationGroupDeleteMutation(graphene.Mutation):
     class Arguments:
         name = graphene.String()
         association = graphene.ID()
 
-    success =  graphene.Boolean()
-    group  = graphene.Field(AssociationGroupType)
+    success = graphene.Boolean()
+    group = graphene.Field(AssociationGroupType)
 
     def mutate(root, info, name, association):
-        _group = AssociationGroup.objects.delete(name=name, association=association)
+        _group = AssociationGroup.objects.delete(name=name,
+                                                 association=association)
 
         return AssociationGroupCreationMutation(group=_group, success=success)
+
+
 class AssociationGroupMemberAddMutation(graphene.Mutation):
     class Arguments:
         memeber = graphene.ID()
@@ -191,24 +219,27 @@ class AssociationGroupMemberAddMutation(graphene.Mutation):
     member = graphene.Field(AssociationGroupMemberType)
 
     def mutate(root, info, member, group):
-        member = AssociationGroupMember.objects.create(member__id=member, group__id=group)
+        member = AssociationGroupMember.objects.create(member__id=member,
+                                                       group__id=group)
         success = True
-        return AssociationGroupMemberAddMutation(member=member, success=success)
+        return AssociationGroupMemberAddMutation(member=member,
+                                                 success=success)
+
+
 # end mutations
 
 
 class AccountsMutation(graphene.ObjectType):
-    add_member =  MemberAddMutation.Field()
+    add_member = MemberAddMutation.Field()
     add_memeber_by_admin = MemberAddByAdminMutation.Field()
     delete_member = MemberDeleteMutation.Field()
     archive_member = MemberArchiveMutation.Field()
 
-    create_association = AssciationCreationMutation.Field()
+    create_association = AssociationCreationMutation.Field()
     update_association_description = AssociationUpdateDescriptionMutation.Field()
-    delete_assciation = AssciationDeleteMutation.Field()
+    delete_assciation = AssociationDeleteMutation.Field()
 
     create_group = AssociationGroupCreationMutation.Field()
     delete_group = AssociationGroupDeleteMutation.Field()
 
     add_members_group = AssociationGroupMemberAddMutation.Field()
-
--- accounts/tests.py
+++ accounts/tests.py
@@ -8,11 +8,12 @@ from .models import Association, AssociationType, ExpectedAssociationMembersNumb
 
 from django.test import RequestFactory
 
+
 class MyFancyTestCase(TestCase):
     databases = {"test_", "default"}
+
     @classmethod
     def setUpTestData(cls):
-        
 
         cls.association_type = AssociationType.objects.create(
             name="sport", description="a sport activitys")
@@ -31,72 +32,124 @@ class MyFancyTestCase(TestCase):
             last_name="abderrahmane",
             email="abderrahmanemustapa@mail.com",
             is_association_owner=False)
-        
+
         cls.delete_user = BaseUser.objects.create(
             first_name="toumidelete_",
             last_name="abderrahmanedelete_",
             email="abderrahmanemustapadelete_@mail.com",
             is_association_owner=False)
 
-        cls.delete_member = Member.objects.create(
-            user=cls.delete_user,
-            association=cls.association)
-        
-        cls.archive_user= BaseUser.objects.create(
+        cls.delete_member = Member.objects.create(user=cls.delete_user,
+                                                  association=cls.association)
+
+        cls.archive_user = BaseUser.objects.create(
             first_name="toumiarchive_",
             last_name="abderrahmanearchive_",
             email="abderrahmanemustapaarchive_@mail.com",
             is_association_owner=False)
-        
-        cls.archive_member = Member.objects.create(
-            user=cls.archive_user,
-            association=cls.association)
+
+        cls.archive_member = Member.objects.create(user=cls.archive_user,
+                                                   association=cls.association)
 
         cls.req = RequestFactory().get('/')
         cls.req.user = cls.user
-    
-    
+
     def test_add_member(self):
         client = Client(schema, context_value=self.req)
 
-        query = "mutation { addMember(association: %s ){success,member{association{name}},}}"%(self.association.id)
-        response = client.execute(query)
-    
-   
-        assert  'errors' not in response 
-    
-    def test_add_member_by_admin(self): 
-        client = Client(schema, context_value=self.req)   
-
-        query = "mutation{addMemeberByAdmin(association:%s, user: \"%s\"){success}}" %(self.association.id, self.user.key)
+        query = "mutation { addMember(association: %s ){success,member{association{name}},}}" % (
+            self.association.id)
         response = client.execute(query)
 
-        assert  'errors' not in response
-    
+        assert 'errors' not in response
+
+    def test_add_member_by_admin(self):
+        client = Client(schema, context_value=self.req)
+
+        query = "mutation{addMemeberByAdmin(association:%s, user: \"%s\"){success}}" % (
+            self.association.id, self.user.key)
+        response = client.execute(query)
+
+        assert 'errors' not in response
+
     def test_delete_member(self):
-        client = Client(schema, context_value=self.req)  
+        client = Client(schema, context_value=self.req)
 
-        query = "mutation{deleteMember(association:%s , user:\"%s\"){success}}" %(self.association.id, self.delete_user.key)
+        query = "mutation{deleteMember(association:%s , user:\"%s\"){success}}" % (
+            self.association.id, self.delete_user.key)
         response = client.execute(query)
-        
+
         member_deleted = Member.objects.filter(
-            user=self.delete_user,
-            association=self.association).exists()
-     
-        assert  'errors' not in response
-        assert member_deleted == False
-    
-    def test_archive_member(self):
-        client = Client(schema, context_value=self.req)  
+            user=self.delete_user, association=self.association).exists()
 
-        query = "mutation{archiveMember(association: %s , user:\"%s\"){success}}" %(self.association.id, self.archive_user.key)
+        assert 'errors' not in response
+        assert member_deleted == False
+
+    def test_archive_member(self):
+        client = Client(schema, context_value=self.req)
+
+        query = "mutation{archiveMember(association: %s , user:\"%s\"){success}}" % (
+            self.association.id, self.archive_user.key)
         response = client.execute(query)
 
-        member_archived = Member.objects.get(
-            user=self.archive_user,
-            association=self.association)
-        
-        assert  'errors' not in response
+        member_archived = Member.objects.get(user=self.archive_user,
+                                             association=self.association)
+
+        assert 'errors' not in response
         assert member_archived.is_archived == True
 
-        
+    def test_create_association(self):
+        client = Client(schema, context_value=self.req)
+
+        query = """mutation {createAssociation(associationMinMaxNumbers: %s , associationType:  %s , 
+  				description:"dqsd", email: "newass@mail.com"
+  				, name: "new asso", phone: "+213780195168")
+                    {
+                            success,association{id,name}
+                    }
+                }""" %( self.association_min_max_numbers.id, self.association_type.id)
+
+        response = client.execute(query)
+        assert 'errors' not in response
+    
+        member_exists =  Member.objects.filter(user=self.user).exists()
+        assert member_exists == True
+
+        member = Member.objects.get(user=self.user)        
+        assert member.is_owner
+
+        assert member.user.is_association_owner
+
+    def test_delete_association(self):
+        client = Client(schema, context_value=self.req)
+
+        query = """
+            mutation {
+                deleteAssciation(id:%s){
+                    success,
+                    association{id,name,description}
+                }
+            }
+        """%(self.association.id)
+
+        response = client.execute(query)
+        assert 'errors' not in response
+    
+    def test_update_association_description(self):
+        client = Client(schema, context_value=self.req)
+        new_association_description =  "new updated description"
+        query= """
+        mutation {
+            updateAssociationDescription(association: %s , description: \"%s\") {
+                success
+                association {
+                    id
+                    name
+                    description
+                }
+            }
+        }
+        """%(self.association.id, new_association_description)
+
+        response = client.execute(query)
+        assert 'errors' not in response
