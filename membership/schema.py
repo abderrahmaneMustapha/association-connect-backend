c5a8fe1673b7b3eb003946cb2a7aa5a12f071b46
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-15 22:02:56 +0100

    feat  :  add form cost graphql muattions

--- membership/schema.py
+++ membership/schema.py
@@ -1,6 +1,6 @@
 from graphene_django import DjangoObjectType
 import graphene
-from .models import Form, Association
+from .models import Form, Association, Costs
 
 
 class FormMetaType(DjangoObjectType):
@@ -11,7 +11,10 @@ class FormMetaType(DjangoObjectType):
             'days'
         ]
 
-
+class CostType(DjangoObjectType):
+    class Meta:
+        model  = Costs
+        fields = ['id', 'form', 'description', 'amount', 'membership_time', 'show_in_form' ]
 #mutations
 
 
@@ -40,9 +43,29 @@ class FormMetaAddMutation(graphene.Mutation):
         success = True
         return FormMetaAddMutation(form=_form, success=success)
 
+class CostMutation(graphene.Mutation):
+    class Arguments:
+        form = graphene.ID()
+        description = graphene.String()
+        amount  = graphene.Float()   
+        membership_time = graphene.String()
+        show_in_form  = graphene.Boolean()
+    
+    cost  =  graphene.Field(CostType)
+    success = graphene.Boolean()
 
+    def mutate(root, info, form, description, amount, membership_time, show_in_form):
+        _form = Form.objects.get(id=form)
+        cost = Costs.objects.create(form=_form, description=description, amount=amount, membership_time=membership_time, show_in_form=show_in_form)
+        cost.full_clean()
+        success = True
+        return CostMutation(cost=cost, success=success)
+
+
+#global query and mutations
 class MembershipMutation(graphene.ObjectType):
     add_form_meta = FormMetaAddMutation.Field()
+    add_cost_to_form = CostMutation.Field()
 
 
 class MembershipQuery(graphene.ObjectType):
