c795e400b156392af5dc4fd412854accc13af18d
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-27 14:37:49 +0100

    test :  update add meta to form

--- membership/tests.py
+++ membership/tests.py
@@ -1,12 +1,16 @@
 from django.test import TestCase
 from graphene.test import Client
 
-from datetime import date,timedelta
+from datetime import date, timedelta
+import textwrap
 
 from backend.schema import schema
 from accounts.models import (Association, AssociationType,
-                             ExpectedAssociationMembersNumber, AssociationMembership, Member)
-from .models import (Form, Association, BaseUser, Member, Costs, FieldType, Field, FieldData, UserPayedCosts)
+                             ExpectedAssociationMembersNumber,
+                             AssociationMembership, Member)
+from .models import (Form, Association, BaseUser, Member, Costs, FieldType,
+                     Field, FieldData, UserPayedCosts)
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class MembershipMutationsTestCase(TestCase):
@@ -14,7 +18,7 @@ class MembershipMutationsTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        
+
         cls.user = BaseUser.objects.create(
             first_name="toumi",
             last_name="abderrahmane",
@@ -41,7 +45,7 @@ class MembershipMutationsTestCase(TestCase):
             email="assoform@mail.com",
             start_date=date.fromisoformat('2020-12-24'),
             days=44)
-        
+
         cls.form_second = Form.objects.create(
             association=cls.association,
             title="Add new members second",
@@ -51,66 +55,95 @@ class MembershipMutationsTestCase(TestCase):
             start_date=date.fromisoformat('2020-12-24'),
             days=44)
 
-        cls.cost  = Costs.objects.create(
-            form = cls.form,
-            description="a new cost of membership", 
-            amount=11, 
-            membership_time=timedelta(days =-1, seconds = 68400), 
-            show_in_form=True
-        )
+        cls.cost = Costs.objects.create(form=cls.form,
+                                        description="a new cost of membership",
+                                        amount=11,
+                                        membership_time=timedelta(
+                                            days=-1, seconds=68400),
+                                        show_in_form=True)
 
-        cls.user_payed_cost = UserPayedCosts.objects.create(user=cls.user, cost=cls.cost)
+        cls.user_payed_cost = UserPayedCosts.objects.create(user=cls.user,
+                                                            cost=cls.cost)
 
         cls.field_type = FieldType.objects.create(name="char")
 
-        cls.field = Field.objects.create(form=cls.form_second, label="azeaze", description="azeaze", placeholder="qazeaze", show_in_form=True,
-               required=True, type=cls.field_type)
+        cls.field = Field.objects.create(form=cls.form_second,
+                                         label="azeaze",
+                                         description="azeaze",
+                                         placeholder="qazeaze",
+                                         show_in_form=True,
+                                         required=True,
+                                         type=cls.field_type)
 
-        cls.field_second = Field.objects.create(form=cls.form, label="new label", description=" new label azeaze", placeholder=" new label qazeaze", show_in_form=True,
-               required=True, type=cls.field_type)
+        cls.field_second = Field.objects.create(
+            form=cls.form,
+            label="new label",
+            description=" new label azeaze",
+            placeholder=" new label qazeaze",
+            show_in_form=True,
+            required=True,
+            type=cls.field_type)
 
-        cls.field_data = FieldData.objects.create(field=cls.field_second, user=cls.user, data={"name": "abderrahmane"})
+        cls.field_data = FieldData.objects.create(
+            field=cls.field_second,
+            user=cls.user,
+            data={"name": "abderrahmane"})
 
     def test_add_form_meta_data(self):
         client = Client(schema)
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04'
+            b'\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'
+            b'\x02\x4c\x01\x00\x3b')
+        image = SimpleUploadedFile(name='test_image.jpg',
+                                   content=small_gif,
+                                   content_type='image/jpeg')
 
-        query = """mutation{
-                    addFormMeta(association:%s, 
-                    days:%s, 
-                    description:\"%s\", 
-                    email:\"%s\", 
-                    startDate:\"%s\", 
-                    title:\"%s\")
-                    {
+        print(image.file)
+        query = textwrap.dedent('''mutation{{
+                    addFormMeta(association:{0}, 
+                    days:{1}, 
+                    description:\"{2}\", 
+                    email:\"{3}\", 
+                    startDate:\"{4}\", 
+                    title:\"{5}\"
+                    link:"https://dzstartup.com",
+                    phone:\"{6}\"
+                    )
+                    {{
                     
-                    form{id,title},
+                    form{{id,title}},
                     success
-                    }
-                }""" % (self.association.id, 55, "azazeazeazeaze",
-                        "abdou@mail.com", date.today(), "New form")
+                    }}
+                }}''').strip("\n").format(self.association.id, 55, "azazeazeazeaze",
+                        "abdou@mail.com", date.today(), "New form",
+                        "+213780195168")
 
         response = client.execute(query)
 
         assert 'errors' not in response
 
-
         query = """mutation{
                     addFormMeta(association:%s, 
                     days:%s, 
                     description:\"%s\", 
                     email:\"%s\", 
                     startDate:\"%s\", 
-                    title:\"%s\")
+                    title:\"%s\",
+                    link:"https://dzstartup.com",
+                    phone:\"%s\"
+                    )
                     {
                     
                     form{id,title, email},
                     success
                     }
-                }""" % (self.association.id, 55, "azazeazeazeaze",
-                        "abdoumail", date.today(), "New form")
+                }""" % (self.association.id, 55, "azazeazeazeaze", "abdoumail",
+                        date.today(), "New form", "+213780195168", )
 
         response = client.execute(query)
-        assert "{'email': ['Enter a valid email address.']}"  in response['errors'][0]['message']
+        assert "{'email': ['Enter a valid email address.']}" in response[
+            'errors'][0]['message']
 
     def test_add_membership_cost_mutation(self):
         client = Client(schema)
@@ -122,11 +155,11 @@ class MembershipMutationsTestCase(TestCase):
                 success
             }
         }""" % (self.form.id, 234, "azazeazeazeaze",
-                        timedelta(days =-1, seconds = 68400) )
-        
+                timedelta(days=-1, seconds=68400))
+
         response = client.execute(query)
-        assert 'errors' not in response 
-    
+        assert 'errors' not in response
+
     def test_add_membership_cost_payed_mutation(self):
         client = Client(schema)
 
@@ -134,11 +167,11 @@ class MembershipMutationsTestCase(TestCase):
             addUserPayedCosts(cost:%s, user:\"%s\"){
                     cost{id, cost{id,description}}
             }
-        }""" % (self.cost.id, self.user.key )
-        
+        }""" % (self.cost.id, self.user.key)
+
         response = client.execute(query)
-        assert 'errors' not in response 
- 
+        assert 'errors' not in response
+
     def test_add_field_to_form_mutation(self):
         client = Client(schema)
 
@@ -151,29 +184,27 @@ class MembershipMutationsTestCase(TestCase):
                 field{id, label},
                 success
             }
-        }""" % (description , self.form.id, label, placeholder, self.field_type.id )
-        
-       
+        }""" % (description, self.form.id, label, placeholder,
+                self.field_type.id)
+
         response = client.execute(query)
-        assert 'errors' not in response 
+        assert 'errors' not in response
 
     def test_add_field_data_to_form_mutation(self):
         client = Client(schema)
 
-        data = { "name" : "this is my field description"}
-      
+        data = {"name": "this is my field description"}
+
         query = """mutation{
             addDataToField(data:\"%s\", field:%s, user:\"%s\"){
                 data{id, field{id, description}},
                 success
             }
-        }""" % (data , self.field.id, self.user.key)
-        
+        }""" % (data, self.field.id, self.user.key)
 
-        
         response = client.execute(query)
-        
-        assert 'errors' not in response 
+
+        assert 'errors' not in response
 
     def test_form_filled_by_user(self):
 
@@ -184,12 +215,14 @@ class MembershipMutationsTestCase(TestCase):
                 success
             }
         }""" % (self.user_payed_cost.id)
-        
+
         response = client.execute(query)
-        assert 'errors' not in response 
-        
-        member_exists = Member.objects.filter(user__key=self.user_payed_cost.user.key).exists()
+        assert 'errors' not in response
+
+        member_exists = Member.objects.filter(
+            user__key=self.user_payed_cost.user.key).exists()
         assert member_exists == True
 
-        membership_exists = AssociationMembership.objects.filter(member__user__key=self.user_payed_cost.user.key).exists()
-        assert  membership_exists
+        membership_exists = AssociationMembership.objects.filter(
+            member__user__key=self.user_payed_cost.user.key).exists()
+        assert membership_exists
