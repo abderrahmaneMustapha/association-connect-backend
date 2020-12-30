742311eb7a6f6adbed615a53d37bf9db47a70994
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-30 11:28:54 +0100

    refactor :  from duration to integer fields for membership time

--- accounts/models.py
+++ accounts/models.py
@@ -129,7 +129,7 @@ class Member (models.Model):
 
 class AssociationMembership(models.Model):
     member = models.ForeignKey(Member, verbose_name=_("member"), on_delete=models.CASCADE)
-    membership_time = models.DurationField(_("membership time "))
+    membership_time = models.IntegerField(_("membership time "))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
