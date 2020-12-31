372ea53220da58e92ed904b93eb47170f68d7a1a
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-12-31 12:58:02 +0100

    refactor :  return all created fields instead of only last one

--- membership/schema.py
+++ membership/schema.py
@@ -202,14 +202,14 @@ class AddFormFieldsMutation(graphene.Mutation):
     class Arguments:
         inputs = graphene.List(FormFieldsInputs)
 
-    field = graphene.Field(FormFieldType)
+    field = graphene.List(FormFieldType)
     success = graphene.Boolean()
     
     def mutate(root, info, inputs):
         _form = Form.objects.filter(
             association__slug=inputs[0].association_slug).first()
         Field.objects.filter(form=_form).delete()
-
+        field = []
         for _input in inputs:
             _field_type = FieldType.objects.get(name=_input.type)
             _field = Field.objects.create(form=_form,
@@ -219,10 +219,11 @@ class AddFormFieldsMutation(graphene.Mutation):
                                           show_in_form=_input.show_in_form,
                                           required=_input.required,
                                           type=_field_type)
+            field.append(_field)
             _field.full_clean()
         success = True
 
-        return AddFormFieldMutation(field=_field, success=success)
+        return AddFormFieldMutation(field=field, success=success)
 
 
 class AddFormFieldMutation(graphene.Mutation):
