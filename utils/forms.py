daec8a30c8dbe36a3ac1f16b9bc2abd05b276a31
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-02-16 19:16:26 +0100

    feat :  add more form to validate data

--- utils/forms.py
+++ utils/forms.py
@@ -9,6 +9,25 @@ class ValidateShortTextFieldForm(forms.Form):
 class ValidateLongTextFieldForm(forms.Form):
     data = forms.CharField(max_length=1500)
 
+class ValidateNumberFieldForm(forms.Form):
+    data = forms.IntegerField()
+
+class ValidateFloatFieldForm(forms.Form):
+    data = forms.FloatField()
+
+class ValidateDateFieldForm(forms.Form):
+    data = forms.DateField()
+
+class ValidateDateTimeFieldForm(forms.Form):
+    data = forms.DateTimeField()
+
+class ValidateTimeFieldForm(forms.Form):
+    data = forms.TimeField()
+
+
+class ValidateDurationFieldForm(forms.Form):
+    data = forms.DurationField()
+
 class ValidateFileFieldForm(forms.Form):
     data = forms.FileField()
 
